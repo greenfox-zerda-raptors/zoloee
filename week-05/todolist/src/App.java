9125cf25606d35e0828ead7c025492dc9a86b7af
zoloee <zoloee@gmail.com>
2016-11-17 14:41:53 +0100

    initial commit

---
+++ week-05/todolist/src/App.java
@@ -0,0 +1,16 @@
+import todolistP.*;
+
+/**
+ * Created by zoloe on 2016. 11. 17..
+ */
+public class App {
+    public static void main(String[] args) {
+        //inicializaias
+        Init();
+
+
+    }
+
+    //inicializaias
+
+}
---
+++ week-05/todolist/src/todolistP/TodoItem.java
@@ -0,0 +1,35 @@
+package todolistP;
+
+/**
+ * Created by zoloe on 2016. 11. 17..
+ */
+public class TodoItem {
+    boolean isDone;
+    String Desc;
+
+    public TodoItem(String desc) {
+        this(false, desc);
+    }
+
+    public TodoItem(boolean isDone, String desc) {
+        this.isDone = isDone;
+        Desc = desc;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getDesc() {
+        return Desc;
+    }
+
+    public void setDesc(String desc) {
+        Desc = desc;
+    }
+}
+}
