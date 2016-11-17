11d48e2305785b67bb1ffa56de208865dc7a5036
zoloee <zoloee@gmail.com>
2016-11-17 21:38:49 +0100

    init list add remove halfway

--- week-05/todolist/src/App.java
+++ week-05/todolist/src/App.java
@@ -1,16 +1,63 @@
+import com.sun.deploy.util.StringUtils;
 import todolistP.*;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Created by zoloe on 2016. 11. 17..
  */
 public class App {
-    public static void main(String[] args) {
-        //inicializaias
-        Init();
+    static Scanner userInput = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String commands = " l to List, q to quit, a to add: ";
+        TodoList myList = new TodoList(); //igy feltolti a default listaval
+//        myList.listTodos();
+
+        String[] command = {" "," "};
+            //initialize commands string to get it empty for the while condition
+
+        while (!command[0].equals("q")) {
+            System.out.println();
+                //add an xtra line
+            System.out.println(commands);
+            command = userInput.nextLine().split(" ",2);
+
+            switch (command[0]){
+                case "l" : {
+                    myList.listTodos();
+                    break;
+                }
+                case "a" : {
+                    if ( command.length < 2 || command[1].equals(" ") ){
+                        System.out.println(" failed - no description");
+                    } else {
+                        myList.addTodo(command[1]);
+                    }
+                    break;
+                }
+                case "q" : {
+                    System.out.println("quitting");
+                    break;
+                }
+                case "r" : {
+                    if ( command.length < 2 || command[1].equals(" ") ){ //ide mas vizsgalat is kell (is.numeric?
+
+                        System.out.println(" failed - no index");
+                    } else {
+                        myList.removeTodo(Integer.getInteger(command[1]));
+                    }
+                    break;
+                }
+                default : {
+                    System.out.println(" no such command. usage: " + commands);
+                    break;
+                }
+            }
+        }
 
     }
-
-    //inicializaias
-
+// || command != "l"
 }
+
