e898caea9520bbef758335ecd8050965cc327c6e
zoloee <zoloee@gmail.com>
2016-11-17 12:04:36 +0100

    moved Instruments to package

--- week-05/day3/src/App.java
+++ week-05/day3/src/App.java
@@ -7,7 +7,7 @@ import music.*;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Test 1, create ElectricGuitar, Bass and Violin with default strings.");
+        System.out.println("Test 1, create music.ElectricGuitar, Bass and music.Violin with default strings.");
         ElectricGuitar guitar = new ElectricGuitar();
         ElectricBassGuitar bassGuitar = new ElectricBassGuitar();
         Violin violin = new Violin();
@@ -17,7 +17,7 @@ public class App {
         bassGuitar.play();
         violin.play();
 
-        System.out.println("Test 2, create ElectricGuitar, Bass with 7 and 5 strings .");
+        System.out.println("Test 2, create music.ElectricGuitar, Bass with 7 and 5 strings .");
         guitar = new ElectricGuitar(7);
         bassGuitar = new ElectricBassGuitar(5);
 
