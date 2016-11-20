8225cfd7c39611964223875819b35e3c8fb40382
zoloee <zoloee@gmail.com>
2016-11-20 12:26:42 +0100

    week-3 folder renaming / git cleaning

---
+++ week-03/day-1/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zoloe on 2016. 11. 02..
+ *
+ * psvm- public static void main(String[] args
+ * sout - System.out.println();
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
