88f18e88a1be9b2472d2853a0002f2161efd9be4
zoloee <zoloee@gmail.com>
2016-11-21 16:57:37 +0100

    w6d1 ws2 - all 3 done, null not working

--- week-06/w6d1-Junit/Test/Ws2ArrayListTest.java
+++ week-06/w6d1-Junit/Test/Ws2ArrayListTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,17 +12,41 @@ import static org.junit.Assert.*;
 public class Ws2ArrayListTest {
     @Test
     public void sum() throws Exception {
-
         Ws2ArrayList myWs2 = new Ws2ArrayList();
         ArrayList<Integer> myIntList = new ArrayList(Arrays.asList(1,5,7,8));
         myWs2.setMyIntList(myIntList);
-        Integer sum2 = 0;
-        for (Integer i :
-                myIntList) {
-            sum2 = sum2 + i;
-        }
-
-        assertEquals(sum2, myWs2.Sum(myIntList));
+        assertEquals(tSum(myIntList), myWs2.Sum(myIntList));
     }
 
+    @Test
+    public void sumIfEmpty() throws Exception {
+        ArrayList<Integer> myEmptyList = new ArrayList<>();
+        Ws2ArrayList myWs2 = new Ws2ArrayList();
+        assertEquals(tSum(myEmptyList), myWs2.Sum(myEmptyList));
+    }
+
+    @Test
+    public void sumIfOneElement() throws Exception {
+        ArrayList<Integer> myIntList = new ArrayList(Arrays.asList(3));
+        Ws2ArrayList myWs2 = new Ws2ArrayList();
+        assertEquals(tSum(myIntList), myWs2.Sum(myIntList));
+    }
+
+    @Test
+    @Ignore
+    public void sumIfNull() throws Exception {
+        Integer[] arr = null;
+        ArrayList<Integer> myNulledList = new ArrayList(Arrays.asList(arr));
+        Ws2ArrayList myWs2 = new Ws2ArrayList();
+        assertEquals(tSum(myNulledList), myWs2.Sum(myNulledList));
+    }
+
+    public Integer tSum(ArrayList<Integer> l){
+        int s = 0;
+        for (Integer i :
+                l) {
+            s = s + i;
+        }
+        return s;
+    }
 }
