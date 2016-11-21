3ef7bbd20473a5b50831176c9cf7195b31272695
zoloee <zoloee@gmail.com>
2016-11-22 00:23:34 +0100

    c02) ready for competition (?)

--- week-06/TheTestingCompetition/tests/TestZolo.java
+++ week-06/TheTestingCompetition/tests/TestZolo.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by zoloe on 2016. 11. 21..
  */
 public class TestZolo {
+    WordToolbox wSource = new WordToolbox ("Tarzan's toenails");
+
+    @Test
+    public void main() throws Exception {
+        assertEquals(wSource.countHowMany('a'),3);
+        assertEquals(wSource.countHowMany('T'),2);
+        assertEquals(wSource.countHowMany('t'),2);
+        assertEquals(wSource.countHowMany('x'),0);
+        assertEquals(wSource.countHowMany(' '),1);
+    }
+
 }
