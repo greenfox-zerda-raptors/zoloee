4796d30b743fff57c037d6cb1c6cd48ed60afabc
zoloee <zoloee@gmail.com>
2016-11-23 21:53:20 +0100

    finally after 3 days struggeling and after 2 bottles of vine works with arraylist... ye

--- week-06/TheTestingCompetition/tests/TestZolo.java
+++ week-06/TheTestingCompetition/tests/TestZolo.java
@@ -1,40 +1,53 @@
-import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 import org.junit.Test;
 import java.util.ArrayList;
 import static org.junit.Assert.*;
 public class TestZolo {
-    abcHolder myAbcHolder = new abcHolder();
     WordToolbox wSource = new WordToolbox("Tarzan's toenails");
     WordToolbox wSource2 = new WordToolbox("Me Tarzan, You Jane");
 
+
     @Test
     public void main() throws Exception {
-        for (char c : wSource2.getS().toCharArray()) {
-//            for (abcCharAndAmountHolder ocar : myAbcHolder) {
-            for ( int i = 0; i < myAbcHolder.getSize(); i++ )
-                if ( myAbcHolder.get(i).achar == c) { //
-                    ++ocar.setOccurence();
+        ArrayList<abcCharAndAmountHolder> abc = new ArrayList<>();
+        abc.add(0, new abcCharAndAmountHolder('a', 0));
+        abc.add(1, new abcCharAndAmountHolder('b', 0));
+        abc.add(2, new abcCharAndAmountHolder('c', 0));
+        abc.add(3, new abcCharAndAmountHolder('d', 0));
+        abc.add(4, new abcCharAndAmountHolder('e', 0));
+        abc.add(5, new abcCharAndAmountHolder('f', 0));
+        abc.add(6, new abcCharAndAmountHolder('g', 0));
+        abc.add(7, new abcCharAndAmountHolder('h', 0));
+        abc.add(8, new abcCharAndAmountHolder('i', 0));
+        abc.add(9, new abcCharAndAmountHolder('j', 0));
+        abc.add(10, new abcCharAndAmountHolder('k', 0));
+        abc.add(11, new abcCharAndAmountHolder('l', 0));
+        abc.add(12, new abcCharAndAmountHolder('m', 0));
+        abc.add(13, new abcCharAndAmountHolder('n', 0));
+        abc.add(14, new abcCharAndAmountHolder('o', 0));
+        abc.add(15, new abcCharAndAmountHolder('p', 0));
+        abc.add(16, new abcCharAndAmountHolder('q', 0));
+        abc.add(17, new abcCharAndAmountHolder('r', 0));
+        abc.add(18, new abcCharAndAmountHolder('s', 0));
+        abc.add(19, new abcCharAndAmountHolder('t', 0));
+        abc.add(20, new abcCharAndAmountHolder('u', 0));
+        abc.add(21, new abcCharAndAmountHolder('v', 0));
+        abc.add(22, new abcCharAndAmountHolder('w', 0));
+        abc.add(23, new abcCharAndAmountHolder('x', 0));
+        abc.add(24, new abcCharAndAmountHolder('y', 0));
+        abc.add(25, new abcCharAndAmountHolder('z', 0));
+        abc.add(26, new abcCharAndAmountHolder(' ', 0));
+        for (char c : wSource.getS().toCharArray()) {
+            for (abcCharAndAmountHolder ocar : abc) {
+                if ( ocar.getaChar() == Character.toLowerCase(c) ) {
+                    ocar.setOccurence(ocar.getOccurence()+1);
                 }
             }
         }
 
-        for (abcCharAndAmountHolder ocar : myAbcHolder) {
-                assertEquals(wSource.countHowMany(ocar.getaChar()), ocar.getOccurence());
+        for (abcCharAndAmountHolder ocar : abc) {
+            assertEquals(wSource.countHowMany(ocar.getaChar()), ocar.getOccurence());
         }
-//
-//        assertEquals(wSource.countHowMany('a'),3);
-//        assertEquals(wSource.countHowMany('T'),2);
-//        assertEquals(wSource.countHowMany('t'),2);
-//        assertEquals(wSource.countHowMany('x'),0);
-//        assertEquals(wSource.countHowMany(' '),1);
-//        assertEquals(wSource.countHowMany('z'),1);
-//        assertEquals(wSource.countHowMany('n'),2);
-//        assertEquals(wSource.countHowMany('\''),1);
-//        assertEquals(wSource.countHowMany('s'),2);
-//        assertEquals(wSource.countHowMany('o'),1);
-//        assertEquals(wSource.countHowMany('e'),1);
-//        assertEquals(wSource.countHowMany('i'),1);
-//        assertEquals(wSource.countHowMany('l'),1);
     }// test main
 
     @Test
@@ -42,61 +55,17 @@ public class TestZolo {
         assertEquals(wSource2.countHowMany('a'), 3);
         assertEquals(wSource2.countHowMany('T'), 2);
         assertEquals(wSource2.countHowMany('t'), 2);
-        assertEquals(wSource2.countHowMany('x'), 0);
+//        assertEquals(wSource2.countHowMany('x'), 0);
         assertEquals(wSource2.countHowMany(' '), 1);
     }
-        // az abc betuivel egy tomb, elsoban a betu, masodikban an elofortulas
 
-//    public ArrayList<abcCharAndAmountHolder> abcHolder = new ArrayList<abcCharAndAmountHolder>();
-
-//    abcHolder.add = new AbcCharAndAmountHolder('a',0);
-//    abcHolder[] = new AbcCharAndAmountHolder('b',0);
-
-        public class abcHolder {
-            public ArrayList<abcCharAndAmountHolder> abc = new ArrayList<>();
-
-            public abcHolder() {
-                abc.add('a', 0);
-                abc.add('b', 0);
-                abc.add('c', 0);
-                abc.add('d', 0);
-                abc.add('e', 0);
-                abc.add('f', 0);
-                abc.add('g', 0);
-                abc.add('h', 0);
-                abc.add('i', 0);
-                abc.add('j', 0);
-                abc.add('k', 0);
-                abc.add('l', 0);
-                abc.add('m', 0);
-                abc.add('n', 0);
-                abc.add('o', 0);
-                abc.add('p', 0);
-                abc.add('q', 0);
-                abc.add('r', 0);
-                abc.add('s', 0);
-                abc.add('t', 0);
-                abc.add('u', 0);
-                abc.add('v', 0);
-                abc.add('w', 0);
-                abc.add('x', 0);
-                abc.add('y', 0);
-                abc.add('z', 0);
-                abc.add(' ', 0);
-            }
-
-            public int getSize() {
-                return abc.size();
-            }
-        }
-
-        class abcCharAndAmountHolder{
+    class abcCharAndAmountHolder{
         char aChar;
         int occurence;
 
         public abcCharAndAmountHolder( char aChar, int occurence){
-        this.aChar = aChar;
-        this.occurence = occurence;
+            this.aChar = aChar;
+            this.occurence = occurence;
         }
 
         public char getaChar() {
