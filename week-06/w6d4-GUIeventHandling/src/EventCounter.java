92a4ce3c13838508f4eea290d0f65aa72a427260
zoloee <zoloee@gmail.com>
2016-11-24 14:33:20 +0100

    w6d4 - all requirements implemented

--- week-06/w6d4-GUIeventHandling/src/EventCounter.java
+++ week-06/w6d4-GUIeventHandling/src/EventCounter.java
@@ -1,21 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Created by zoloe on 2016. 11. 24..
  */
 public class EventCounter extends JFrame {
+    int counterKcount = 0;
+    int counterMcount = 0;
+    int counterWcount = 0;
+    JTextField TextField = new JTextField(20);
+    JLabel mCount = new JLabel("-");
+
+    JLabel kCount = new JLabel("-");
+
+    JLabel wCount = new JLabel("-");
+
+
+
     public EventCounter() throws HeadlessException {
-        JTextField TextField = new JTextField(20);
-        JLabel mCount = new JLabel("-");
+
         JLabel nameMcount = new JLabel("Mouse counter:");
         nameMcount.setLabelFor(mCount);
-        JLabel kCount = new JLabel("-");
         JLabel nameKcount = new JLabel("Keyboard counter:");
         nameKcount.setLabelFor(kCount);
-        JLabel wCount = new JLabel("-");
         JLabel nameWcount = new JLabel("Window counter:");
         nameWcount.setLabelFor(nameWcount);
+
+        TextField.addKeyListener(new ListenForKeys());
+        this.addMouseListener(new ListenForMouse());
+        this.addWindowListener(new listenForWindow());
 
         Box ecBox = Box.createVerticalBox();
 
@@ -43,4 +57,97 @@ public class EventCounter extends JFrame {
 
     }
 
+    private class ListenForKeys implements KeyListener{
+        @Override
+        public void keyPressed(KeyEvent e) {
+//            setCounter(counterKcount,kCount);  // valamiert ez csak egyszer fut
+            counterKcount++;
+            kCount.setText(Integer.toString(counterKcount));
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            counterKcount++;
+            kCount.setText(Integer.toString(counterKcount));
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+    }
+    private void setCounter(int counter, JLabel label){
+        counter++;
+        label.setText(Integer.toString(counter));
+    }
+
+    private class ListenForMouse implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            counterMcount++;
+            mCount.setText(Integer.toString(counterMcount));
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
+
+    private class listenForWindow implements WindowListener {
+        @Override
+        public void windowOpened(WindowEvent e) {
+            counterWcount++;
+            wCount.setText(Integer.toString(counterWcount));
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowClosed(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowIconified(WindowEvent e) {
+            counterWcount++;
+            wCount.setText(Integer.toString(counterWcount));
+        }
+
+        @Override
+        public void windowDeiconified(WindowEvent e) {
+            counterWcount++;
+            wCount.setText(Integer.toString(counterWcount));
+        }
+
+        @Override
+        public void windowActivated(WindowEvent e) {
+            counterWcount++;
+            wCount.setText(Integer.toString(counterWcount));
+        }
+
+        @Override
+        public void windowDeactivated(WindowEvent e) {
+            counterWcount++;
+            wCount.setText(Integer.toString(counterWcount));
+        }
+    }
 }
