b68aef5e885ef14303b1c4d23641552bc97b767f
zoloee <zoloee@gmail.com>
2016-11-24 13:44:10 +0100

    w6d4 - initial setup (box with labels)

---
+++ week-06/w6d4-GUIeventHandling/src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * Created by zoloe on 2016. 11. 24..
+ */
+public class App {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                new EventCounter();
+            }
+
+        });
+    }
+}
---
+++ week-06/w6d4-GUIeventHandling/src/EventCounter.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by zoloe on 2016. 11. 24..
+ */
+public class EventCounter extends JFrame {
+    public EventCounter() throws HeadlessException {
+    JTextField TextField = new JTextField(20);
+    JLabel mCount = new JLabel("-");
+       JLabel nameMcount = new JLabel("Mouse counter:");
+    JLabel kCount = new JLabel("-");
+        JLabel nameKcount = new JLabel("Keyboard counter:");
+    JLabel wCount = new JLabel("-");
+        JLabel nameWcount = new JLabel("Window counter:");
+
+    Box ecBox = Box.createVerticalBox();
+
+        ecBox.add(Box.createVerticalStrut(10));
+        ecBox.add(TextField);
+        ecBox.add(Box.createVerticalStrut(10));
+        ecBox.add(nameMcount);
+        ecBox.add(mCount);
+        ecBox.add(Box.createVerticalStrut(10));
+        ecBox.add(nameKcount);
+        ecBox.add(kCount);
+        ecBox.add(Box.createVerticalStrut(10));
+        ecBox.add(nameWcount);
+        ecBox.add(wCount);
+        ecBox.add(Box.createVerticalStrut(10));
+
+        this.setTitle("EventCounter");
+        this.add(ecBox, BorderLayout.CENTER);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+//        this.setSize(400,400);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+}
