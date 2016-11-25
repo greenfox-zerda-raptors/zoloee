e27eeab240acd3a7023dc847667e9a7d54448c3c
zoloee <zoloee@gmail.com>
2016-11-25 11:25:25 +0100

    w6d4 - image - imagePanel draws the background

--- week-06/w6d4-GUIeventHandling/src/ImagePanel.java
+++ week-06/w6d4-GUIeventHandling/src/ImagePanel.java
@@ -5,7 +5,16 @@ import java.awt.*;
  * Created by zoloe on 2016. 11. 24..
  */
 public class ImagePanel extends JPanel {
+    private Image myImage;
     public ImagePanel() {
-        Toolkit.getDefaultToolkit().createImage("image_example.png");
+        myImage = Toolkit.getDefaultToolkit().createImage("image_example.png");
     }
+    @Override
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+//        requestFocus();
+        graphics.drawImage(myImage, 0, 0, this);
+    }
+
+
 }
