2f7d36f765d625d33b6d7828e07eb03390c239d5
zoloee <zoloee@gmail.com>
2016-12-05 17:20:37 +0100

    cleaned up the loops

--- week-07/Scrum-game/src/Board.java
+++ week-07/Scrum-game/src/Board.java
@@ -14,16 +14,11 @@ public class Board extends JPanel{
         tiles = new ArrayList<>();
         for (int j = 0; j < 720; j += 72) {
             for (int k = 0; k < 720; k += 72) {
-//                if (k % 350 == 0) {
-//                    Tile image = new Tile("images/wall.png", k, j);
-//                    tiles.add(image);
-//                }else {
+
                 Tile image = new Tile("images/floor.png", k, j);
-                tiles.add(image);
+                tiles.add(image); // tiles.add(new Floor[k][j]
             }
         }
-//    }
-        //ide folhozni a for
 
         // set the size of your draw board
         setPreferredSize(new Dimension(720, 900));
@@ -36,26 +31,9 @@ public class Board extends JPanel{
         // here you have a 700x900 canvas
         // you can create and draw an image using the class below e.g.
 
-//        Tile image = new Tile("images/floor.png", 0, 0);
-//        image.draw(graphics);
-
-
         for (Tile tile :
              tiles) {
             tile.draw(graphics);
         }
-
-//        for (int j = 0; j < 720; j += 72 ){
-//            for (int k = 0; k < 720; k += 72 ){
-//                if ( k % 350 == 0 ) {
-//                    Tile image = new Tile("images/wall.png", k, j );
-//                    image.draw(graphics);
-//                }else {
-//                    Tile image = new Tile("images/floor.png", k, j );
-//                    image.draw(graphics);
-//                }
-//
-//            }
-//        }
     }//paint
 }//Board
---
+++ week-07/Scrum-game/src/Floor.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zoloe on 2016. 12. 05..
+ */
+public class Floor extends Tile {
+
+    public Floor(String filename, int posX, int posY) {
+        super("images/wall.png", posX, posY);
+    }
+}
