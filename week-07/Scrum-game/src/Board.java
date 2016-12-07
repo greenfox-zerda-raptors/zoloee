19b8b2859be146673879c15b45bcb332d690d875
zoloee <zoloee@gmail.com>
2016-12-07 10:16:45 +0100

    added keyListener skeleton

--- week-07/Scrum-game/src/Board.java
+++ week-07/Scrum-game/src/Board.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 
@@ -9,8 +10,9 @@ import java.util.ArrayList;
 public class Board extends JPanel{
 
     ArrayList<GameObject> tiles;
+    GameObject myHero;
     int[][] map = new int[][]{
-            {2,1,1,1,1,1,1,1,1,1},
+            {0,1,1,1,1,1,1,1,1,1},
             {0,1,1,0,0,0,0,0,0,0},
             {0,0,0,0,1,1,0,1,0,0},
             {0,1,1,0,0,1,0,1,0,0},
@@ -31,17 +33,19 @@ public class Board extends JPanel{
                     tiles.add(new Floor(j,i));
                 }else if ( map[i][j] == 1 ){
                     tiles.add(new Wall(j,i)); // pos x is the horizontal axle
-                }else if ( map[i][j] == 2 ){
-                    tiles.add(new Floor(j,i));
-                    tiles.add(new Hero(j,i));
+//                }else if ( map[i][j] == 2 ){
+//                    tiles.add(new Floor(j,i));
+//                    tiles.add(new Hero(j,i));
                 }
             }
         }
+        myHero = new Hero(0,0);
 
         // set the size of your draw board
         setPreferredSize(new Dimension(720, 900));
         setVisible(true);
     }
+
 
     @Override
     public void paint(Graphics graphics){
@@ -53,5 +57,36 @@ public class Board extends JPanel{
                 tiles) {
             tile.draw(graphics);
         }
+        myHero.draw(graphics);
     }//paint
+
+    class myKeyListener implements KeyListener {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_DOWN : {
+                    myHero = new Hero(0,0);
+                    break;
+                }
+                case KeyEvent.VK_RIGHT : {
+                    break;
+                }
+                default:{
+                    break;
+                }
+            }
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+    }// myKeyListener
 }//Board
