01e7a048609dac5750093156951096c3b9e78b07
zoloee <zoloee@gmail.com>
2016-12-19 14:49:45 +0100

    counter with atomicLong

--- week-09/w9d1-springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-09/w9d1-springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,14 +5,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by zoloe on 2016. 12. 19..
  */
 @RestController
 public class HelloRESTController {
+    final AtomicLong counter = new AtomicLong (1);
+
     @RequestMapping(value = "/greeting")
     @ResponseBody
     public Greeting greeting(@RequestParam() String name){
-        return new Greeting(1,name);
+        long id = counter.incrementAndGet();
+        return new Greeting(id,"Hello, " + name + "!");
     }
 }
