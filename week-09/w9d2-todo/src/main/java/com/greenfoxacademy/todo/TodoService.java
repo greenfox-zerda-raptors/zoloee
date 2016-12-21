05355813dfc5ef3f21d22d6fdf89f39b0eeccc4f
zoloee <zoloee@gmail.com>
2016-12-21 16:20:32 +0100

    "Todos"  working (default TodoList in TodoService must be static)

--- week-09/w9d2-todo/src/main/java/com/greenfoxacademy/todo/TodoService.java
+++ week-09/w9d2-todo/src/main/java/com/greenfoxacademy/todo/TodoService.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,14 +14,11 @@ import java.util.List;
 @Getter
 @Service
 public class TodoService {
-    List<Todo> todos = new ArrayList<>();
-    public void todoService(){
-        todos.add(0,new Todo(1,"Walk the dog",false,false ));
-        todos.add(1,new Todo(2,"Buy Milk", false,true));
-        todos.add(2,new Todo(3,"Do homework",true,true));
-//        System.out.println("list initialized with 3 example values");
+    List<Todo> todos = new ArrayList<>(
+            Arrays.asList(
+                    new Todo(1,"Walk the dog",false,false ),
+                    new Todo(2,"Buy Milk", false,true),
+                    new Todo(3,"Do homework",true,true)));
     }
-
-
     //    Use getTodos() from TodoService to get all the Todos
-}
+
