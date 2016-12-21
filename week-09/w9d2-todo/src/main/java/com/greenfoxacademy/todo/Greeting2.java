c57e5935c37726752cb6d72adc484ee2221243e8
zoloee <zoloee@gmail.com>
2016-12-21 14:46:02 +0100

    "Hello Templates" done

---
+++ week-09/w9d2-todo/src/main/java/com/greenfoxacademy/todo/Greeting2.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.todo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Created by zoloe on 2016. 12. 21..
+ */
+
+@AllArgsConstructor
+@Getter
+//@ToString
+public class Greeting2 {
+//    long id;
+    String content;
+}
--- week-09/w9d2-todo/src/main/java/com/greenfoxacademy/todo/HelloController.java
+++ week-09/w9d2-todo/src/main/java/com/greenfoxacademy/todo/HelloController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.todo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,11 +12,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @RequestMapping(value = "/helloWorld")
-    @ResponseBody
-    public Greeting greeting(@RequestParam() String name){
-        long id = counter.incrementAndGet();
-        return new Greeting(id,"Hello, " + name + "!");
+    public String greeting(Model model, @RequestParam(required = false, defaultValue = "Thymeleaf")  String name){
+        model.addAttribute("name", name);
+//        Hello, " + name + "!
+        return "greeting";
     }
 }
 
-}
+//    @RequestMapping("/web/greeting")
+//    public String greeting(Model model, @RequestParam String name) {
+//        long id = counter.incrementAndGet();
+//        model.addAttribute("name", name);
+//        model.addAttribute("count", id);
+//        return "greeting";
+//    }
+//}
+//public class HelloController {
+//    @RequestMapping(value = "/helloWorld")
+//    @ResponseBody
+//    public Greeting2 greeting(Model model, @RequestParam(required = false, defaultValue = "Thymeleaf")  String name){
+////        model.addAttribute("name", name);
+////        Hello, " + name + "!
+//        return new Greeting2("xx " + name );
+//    }
+//}
