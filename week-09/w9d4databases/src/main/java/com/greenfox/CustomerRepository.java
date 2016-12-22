d3f95bd17510ad076e61d70c4a55ba31bb5b1897
zoloee <zoloee@gmail.com>
2016-12-22 11:13:45 +0100

    initial commit

---
+++ week-09/w9d4databases/src/main/java/com/greenfox/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.greenfox;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+}
