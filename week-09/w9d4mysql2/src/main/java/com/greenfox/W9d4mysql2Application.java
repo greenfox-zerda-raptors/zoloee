5e933ffa21baed4b17805e293e4c5231280f9bfa
zoloee <zoloee@gmail.com>
2017-01-02 21:50:04 +0100

    empty spring ijitialializr with JPA

---
+++ week-09/w9d4mysql2/src/main/java/com/greenfox/W9d4mysql2Application.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W9d4mysql2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W9d4mysql2Application.class, args);
+	}
+}
