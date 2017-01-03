4a4cc5aa57465d3adb48f4d5e8bcf1c8668ac1c0
zoloee <zoloee@gmail.com>
2017-01-03 16:51:50 +0100

    tennisGame1 refactored

---
+++ week-10/Tennis-Refactoring-Kata-master/src/main/java/Player.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zoloe on 2017. 01. 03..
+ */
+public class Player {
+    protected int score = 0;
+    protected String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
