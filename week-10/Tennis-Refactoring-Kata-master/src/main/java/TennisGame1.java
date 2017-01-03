e1f4599e872518aacc4a2d405b66b4c9ef4e47e6
zoloee <zoloee@gmail.com>
2017-01-03 16:52:21 +0100

    tennisGame1 refactored

--- week-10/Tennis-Refactoring-Kata-master/src/main/java/TennisGame1.java
+++ week-10/Tennis-Refactoring-Kata-master/src/main/java/TennisGame1.java
@@ -1,76 +1,90 @@
+import static java.lang.Math.abs;
 
 public class TennisGame1 implements TennisGame {
+    private Player player1;
+    private Player player2;
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    public TennisGame1(String player1name, String player2Name) {
+        player1 = new Player(player1name);
+        player2 = new Player(player2Name);
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(player1.name))
+            player1.score += 1;
         else
-            m_score2 += 1;
+            player2.score += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+        if (thereIsAwinner()) {
+            score = "Win for " + theBetterPlayer();
+        } else if (thereIsAdvantage()) {
+            score = "Advantage " + theBetterPlayer();
+        } else if (thereIsDeuce()) {
+            score = "Deuce";
+        } else {
+            score = scoreNaming(player1.score) + "-";
+            if (scoresEqual()) {
+                score += "All";
+            } else {
+                score += scoreNaming(player2.score);
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return score;
+    }
+    
+    private boolean scoreDifferenceIsMoreThanOne(){
+        return (abs(player1.score - player2.score) >= 2);
+    }
+    private boolean scoreDifferenceIsOne(){
+        return (abs(player1.score - player2.score) == 1);
+    }
+    private boolean scoresEqual(){
+        return (player1.score == player2.score);
+    }
+    private boolean fourPointsReached(){
+        return (Math.max(player1.score, player2.score) > 3);
+    }
+    private boolean threePointsReached(){
+        return (Math.max(player1.score, player2.score) > 2);
+    }
+    private boolean thereIsAwinner() {
+        return (scoreDifferenceIsMoreThanOne() && fourPointsReached());
+    }
+    private boolean thereIsDeuce() {
+        return (scoresEqual() && threePointsReached());
+    }
+    private boolean thereIsAdvantage() {
+        return (fourPointsReached() && scoreDifferenceIsOne());
+    }
+    private String theBetterPlayer(){
+        if (player1.score > player2.score) {
+            return player1.name;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
+        else {
+            return player2.name;
+        }
+    }
+    private String scoreNaming(int playerScore) {
+        String score="";
+        switch(playerScore)
                 {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+                case 0:
+                score = "Love";
+                break;
+                case 1:
+                score = "Fifteen";
+                break;
+                case 2:
+                score = "Thirty";
+                break;
+                case 3:
+                score = "Forty";
+                break;
                 }
-            }
-        }
         return score;
     }
 }
