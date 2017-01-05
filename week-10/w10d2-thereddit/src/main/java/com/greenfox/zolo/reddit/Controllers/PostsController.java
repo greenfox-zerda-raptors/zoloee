bc77860775881ef274519f84093aeec9e740ec61
zoloee <zoloee@gmail.com>
2017-01-05 12:39:45 +0100

    added add new post

--- week-10/w10d2-thereddit/src/main/java/com/greenfox/zolo/reddit/Controllers/PostsController.java
+++ week-10/w10d2-thereddit/src/main/java/com/greenfox/zolo/reddit/Controllers/PostsController.java
@@ -1,12 +1,11 @@
 package com.greenfox.zolo.reddit.Controllers;
 
+import com.greenfox.zolo.reddit.Models.Post;
 import com.greenfox.zolo.reddit.Services.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by zoloe on 2017. 01. 04..
@@ -26,6 +25,14 @@ public class PostsController
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add(Model model){
+        model.addAttribute("post", new Post());
         return "posts/add";
     }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addSubmit(@ModelAttribute Post post){
+        repository.save(post);
+        return "redirect:/";
+    }
+
 }
