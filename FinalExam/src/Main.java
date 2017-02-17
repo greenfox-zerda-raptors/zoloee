9d05629bf0bc529de11ee69f12b711cab428ab06
zoloee <zoloee@gmail.com>
2017-02-17 10:12:47 +0100

    1st version

---
+++ FinalExam/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Util util = new Util();
+        int[] list = new int[]{1, 3, 8, 6, 7, 4};
+        int[] list2 = new int[]{1, 9, 8, 6, 7, 8};
+
+//        int x = util.getBiggestFromPairs(list);
+        int y = util.getBiggestFromPairs(list2);
+//        System.out.println(x);
+        System.out.println(y);
+
+    }
+
+
+
+}
---
+++ FinalExam/src/Util.java
@@ -0,0 +1,26 @@
+
+//Create a function that takes an array of integers
+// and retunrs the biggest from each of its second elements
+// like: [1, 3, 8, 6, 7, 4] would return 6
+/**
+ * Created by zoloe on 2017. 02. 17..
+ */
+public class Util {
+
+    public Util() {
+    }
+
+    public Integer getBiggestFromPairs(int[] ints){
+        int length = ints.length;
+        int biggest = ints[0];
+
+        for (int i = 1; i < length-2; i=i+2){
+            if (ints[i] > ints[i+2]) {
+                biggest = ints[i];
+            }else {
+                biggest = ints[i+2];
+            }
+        }
+        return biggest;
+    }
+}
